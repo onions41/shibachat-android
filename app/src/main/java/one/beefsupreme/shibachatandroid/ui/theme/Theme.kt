40036a245c5b9ba94bb24e419ffbdaeee6ec979f
onions41 <kblee89@outlook.com>
2022-10-08 00:19:59 -0700

    Added new colors to theme

--- app/src/main/java/one/beefsupreme/shibachatandroid/ui/theme/Theme.kt
+++ app/src/main/java/one/beefsupreme/shibachatandroid/ui/theme/Theme.kt
@@ -7,15 +7,17 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-  primary = Purple200,
-  primaryVariant = Purple700,
-  secondary = Teal200
+  primary = Orange,
+  primaryVariant = OrangeDark,
+  secondary = Teal200,
+  surface = OrangeDarker
 )
 
 private val LightColorPalette = lightColors(
-  primary = Purple500,
-  primaryVariant = Purple700,
-  secondary = Teal200
+  primary = Orange,
+  primaryVariant = OrangeDark,
+  secondary = Teal200,
+  surface = OrangeWhite
 
   /* Other default colors to override
     background = Color.White,
