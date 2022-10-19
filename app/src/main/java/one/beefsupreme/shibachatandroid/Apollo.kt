4523a45f6b620925bcb04bb62293aec40a76a4c4
onions41 <kblee89@outlook.com>
2022-10-18 18:55:50 -0700

    Added AuthInterceptor to Apollo.kt

--- app/src/main/java/one/beefsupreme/shibachatandroid/Apollo.kt
+++ app/src/main/java/one/beefsupreme/shibachatandroid/Apollo.kt
@@ -1,7 +1,67 @@
 package one.beefsupreme.shibachatandroid
 
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.network.okHttpClient
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+
+// accessToken
+object AccessTokenStorage {
+  private var accessToken: String = "This is the sample access token blah"
+  fun getAccessTok(): String = accessToken
+  fun setAccessTok(newToken: String) {
+    accessToken = newToken
+  }
+}
+
+// Interceptor class
+class AuthInterceptor: Interceptor {
+
+  override fun intercept(chain: Interceptor.Chain): Response {
+    val request = chain.request()
+    val accessToken = AccessTokenStorage.getAccessTok()
+
+    val response = chain.proceed(newRequestWithAccessToken(accessToken, request))
+
+//    if (response.code == HttpURLConnection.HTTP_UNAUTHORIZED) {
+//      val newAccessToken = AccessTokenStorage.getAccessTok()
+//      if (newAccessToken != accessToken) {
+//        return chain.proceed(newRequestWithAccessToken(accessToken, request))
+//      } else {
+//        accessToken = refreshToken()
+//        if (accessToken.isNullOrBlank()) {
+//          sessionManager.logout()
+//          return response
+//        }
+//        return chain.proceed(newRequestWithAccessToken(accessToken, request))
+//      }
+//    }
+
+    return response
+  }
+
+  private fun newRequestWithAccessToken(accessToken: String, request: Request): Request =
+    request.newBuilder()
+      .header("access-token", accessToken)
+      .build()
+
+//  private fun refreshToken(): String? {
+//    synchronized(this) {
+//      val refreshToken = sessionManager.getRefreshToken()
+//      refreshToken?.let {
+//        return sessionManager.refreshToken(refreshToken)
+//      } ?: return null
+//    }
+//  }
+}
+
+val okHttpClient = OkHttpClient.Builder()
+  .addNetworkInterceptor(AuthInterceptor())
+  .build()
 
 val apolloClient = ApolloClient.Builder()
   .serverUrl("${BuildConfig.SERVER_URL}/graphql")
+  .okHttpClient(okHttpClient)
   .build()
