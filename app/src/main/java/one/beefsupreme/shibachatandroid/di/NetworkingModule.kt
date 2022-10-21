c56db02eb722dd9925f7648cebd22bbd307d596a
onions41 <kblee89@outlook.com>
2022-10-21 11:43:17 -0700

    Bind TokenRefreshInterceptor and created another OkHttpClient binding to inject into TokenRefreshInterceptor

--- app/src/main/java/one/beefsupreme/shibachatandroid/di/NetworkingModule.kt
+++ app/src/main/java/one/beefsupreme/shibachatandroid/di/NetworkingModule.kt
@@ -7,48 +7,98 @@ import com.franmontiel.persistentcookiejar.ClearableCookieJar
 import com.franmontiel.persistentcookiejar.PersistentCookieJar
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache
 import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import one.beefsupreme.shibachatandroid.interceptors.AuthInterceptor
 import one.beefsupreme.shibachatandroid.BuildConfig
+import one.beefsupreme.shibachatandroid.interceptors.AuthInterceptorImpl
+import one.beefsupreme.shibachatandroid.interceptors.TokenRefreshInterceptorImpl
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class AuthInterceptor
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class TokenRefreshInterceptor
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ApolloOkHttpClient
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class TokenRefreshOkHttpClient
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkingModule {
+abstract class NetworkingModule {
 
-  @Provides
+  @AuthInterceptor
+  @Binds
   @Singleton
-  fun provideCookieJar(app: Application): ClearableCookieJar {
-    return PersistentCookieJar(
-      SetCookieCache(),
-      SharedPrefsCookiePersistor(app.applicationContext)
-    )
-  }
+  abstract fun bindAuthInterceptor(
+    authInterceptorImpl: AuthInterceptorImpl
+  ): Interceptor
 
-  @Provides
+  @TokenRefreshInterceptor
+  @Binds
   @Singleton
-  fun provideOkHttpClient(
-    cookieJar: ClearableCookieJar,
-    authInterceptor: AuthInterceptor
-  ): OkHttpClient {
-    return OkHttpClient.Builder()
-      .addNetworkInterceptor(authInterceptor)
-      .cookieJar(cookieJar)
-      .build()
-  }
+  abstract fun bindTokenRefreshInterceptor(
+    tokenRefreshInterceptorImpl: TokenRefreshInterceptorImpl
+  ): Interceptor
 
-  @Provides
-  @Singleton
-  fun provideApolloClient(
-    okHttpClient: OkHttpClient
-  ): ApolloClient {
-    return ApolloClient.Builder()
-      .serverUrl("${BuildConfig.SERVER_URL}/graphql")
-      .okHttpClient(okHttpClient)
-      .build()
+  internal companion object{
+    @Provides
+    @Singleton
+    fun provideCookieJar(app: Application): ClearableCookieJar {
+      return PersistentCookieJar(
+        SetCookieCache(),
+        SharedPrefsCookiePersistor(app.applicationContext)
+      )
+    }
+
+    @TokenRefreshOkHttpClient
+    @Provides
+    @Singleton
+    fun provideTokenRefreshOkHttpClient(
+      cookieJar: ClearableCookieJar,
+    ): OkHttpClient {
+      return OkHttpClient.Builder()
+        .cookieJar(cookieJar)
+        .build()
+    }
+
+    @ApolloOkHttpClient
+    @Provides
+    @Singleton
+    fun provideApolloOkHttpClient(
+      cookieJar: ClearableCookieJar,
+      @TokenRefreshInterceptor tokenRefreshInterceptor: Interceptor,
+      @AuthInterceptor authInterceptor: Interceptor
+    ): OkHttpClient {
+      return OkHttpClient.Builder()
+        .cookieJar(cookieJar)
+        .addNetworkInterceptor(tokenRefreshInterceptor)
+        .addNetworkInterceptor(authInterceptor)
+        .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApolloClient(
+      @ApolloOkHttpClient okHttpClient: OkHttpClient
+    ): ApolloClient {
+      return ApolloClient.Builder()
+        .serverUrl("${BuildConfig.SERVER_URL}/graphql")
+        .okHttpClient(okHttpClient)
+        .build()
+    }
   }
 }
