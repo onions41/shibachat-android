03be5d1465202621e85aadd50211b796d809e443
onions41 <kblee89@outlook.com>
2022-10-23 01:43:15 -0700

    MovedMeViewModel into /viewmodels

--- app/src/main/java/one/beefsupreme/shibachatandroid/ui/viewmodels/MeViewModel.kt
+++ app/src/main/java/one/beefsupreme/shibachatandroid/ui/viewmodels/MeViewModel.kt
@@ -1,19 +1,19 @@
-package one.beefsupreme.shibachatandroid.ui
+package one.beefsupreme.shibachatandroid.ui.viewmodels
 
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.apollographql.apollo3.ApolloClient
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import one.beefsupreme.shibachatandroid.AccessTokenStorage
 import one.beefsupreme.shibachatandroid.BingBongQuery
 import one.beefsupreme.shibachatandroid.LoginMutation
 import one.beefsupreme.shibachatandroid.ProtectedQuery
 import one.beefsupreme.shibachatandroid.UnprotectedQuery
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import com.apollographql.apollo3.ApolloClient
-import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 private const val TAG = "**MeViewModel**"
