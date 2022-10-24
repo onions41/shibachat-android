4b3d09170268ba53148e5f9982c30352fb7c876d
onions41 <kblee89@outlook.com>
2022-10-23 23:22:38 -0700

    Added DestinationNavHost to App composable

--- app/src/main/java/one/beefsupreme/shibachatandroid/ui/App.kt
+++ app/src/main/java/one/beefsupreme/shibachatandroid/ui/App.kt
@@ -1,13 +1,12 @@
 package one.beefsupreme.shibachatandroid.ui
 
 import androidx.compose.runtime.Composable
-import one.beefsupreme.shibachatandroid.ui.screens.FirstScreen
+import com.ramcosta.composedestinations.DestinationsNavHost
 import one.beefsupreme.shibachatandroid.ui.theme.ShibachatAndroidTheme
 
 @Composable
 fun App() {
   ShibachatAndroidTheme {
-    // Todo: NavHost goes here
-    FirstScreen()
+    DestinationsNavHost(navGraph = NavGraphs.root)
   }
 }
