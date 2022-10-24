d004497b7d8666c79b731ded5372dfef243e7391
onions41 <kblee89@outlook.com>
2022-10-23 23:15:24 -0700

    Deleted AccessTokenStorage. Want to implement this in a repo instead and inject

--- app/src/main/java/one/beefsupreme/shibachatandroid/AccessTokenStorage.kt
+++
@@ -1,9 +0,0 @@
-package one.beefsupreme.shibachatandroid
-
-object AccessTokenStorage {
-  private var accessToken: String = ""
-  fun getAccessTok(): String = accessToken
-  fun setAccessTok(newToken: String) {
-    accessToken = newToken
-  }
-}
