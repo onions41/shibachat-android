258b771024856f8942f77a1b200967fb4a1116fa
onions41 <kblee89@outlook.com>
2022-11-03 17:27:57 -0700

    Added doneInitialTokenFetch property to LoginStateImpl

--- app/src/main/java/one/beefsupreme/shibachatandroid/repo/LoginStateImpl.kt
+++ app/src/main/java/one/beefsupreme/shibachatandroid/repo/LoginStateImpl.kt
@@ -5,24 +5,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 
 /**
- * We can only know that we have been deemed logged in or logged out by the server at 3 occasions:
- * 1. At Login mutation.
- * 2. When TokenRefreshInterceptor fires because our token is expired.
- * 3. At app mount.
- * 4. Logout...
- * we are the spanish inq...
- *
  * The response from /refresh-token will contain "" as the accessToken
  * Basically, if the accessToken is "" it should also be logged out. I just don't want to usee the
  * AccessToken string as a state, which is why I separated it like this
  */
-
 class LoginStateImpl {
   var isLoggedIn by mutableStateOf(false)
     private set
 
   var accessToken: String = ""
     private set
+
+  // Used to make sure the initial token fetch is only done
+  // after a process kill or finish (), which would reset this to false.
+  var doneInitialTokenFetch: Boolean = false
 
   fun login(newAccessToken: String) {
     isLoggedIn = true
