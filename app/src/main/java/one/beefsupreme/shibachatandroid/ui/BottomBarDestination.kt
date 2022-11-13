88e6796b8c3ad2db2e964571566e73ac0e45447d
onions41 <kblee89@outlook.com>
2022-11-12 20:35:56 -0800

    Created bottom bar needed in @App

---
+++ app/src/main/java/one/beefsupreme/shibachatandroid/ui/BottomBarDestination.kt
@@ -0,0 +1,22 @@
+package one.beefsupreme.shibachatandroid.ui
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.ramcosta.composedestinations.spec.DirectionDestinationSpec
+import one.beefsupreme.shibachatandroid.R
+import one.beefsupreme.shibachatandroid.ui.destinations.AccountScreenDestination
+import one.beefsupreme.shibachatandroid.ui.destinations.FriendsScreenDestination
+import one.beefsupreme.shibachatandroid.ui.destinations.HomeScreenDestination
+
+enum class BottomBarDestination(
+  val direction: DirectionDestinationSpec,
+  val icon: ImageVector,
+  @StringRes val label: Int
+) {
+  Home(HomeScreenDestination, Icons.Default.Home, R.string.home_screen),
+  Friends(FriendsScreenDestination, Icons.Default.Email, R.string.friends_screen),
+  Account(AccountScreenDestination, Icons.Default.Home, R.string.account_screen),
+}
