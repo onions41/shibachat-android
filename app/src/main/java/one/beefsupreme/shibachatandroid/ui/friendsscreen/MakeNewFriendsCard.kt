4f701669a58bd4545101c6bb22cdf13a2088f59f
onions41 <kblee89@outlook.com>
2022-11-16 13:50:41 -0800

    Created MakeNewFriendsCard (link) and ReceivedFReqCard

---
+++ app/src/main/java/one/beefsupreme/shibachatandroid/ui/friendsscreen/MakeNewFriendsCard.kt
@@ -0,0 +1,62 @@
+package one.beefsupreme.shibachatandroid.ui.friendsscreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import one.beefsupreme.shibachatandroid.R
+import one.beefsupreme.shibachatandroid.ui.theme.ShibachatAndroidTheme
+
+@Composable
+fun MakeNewFriendsCard(handleClick: () -> Unit) {
+  Row(
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = Modifier
+      .fillMaxWidth()
+      .height(65.dp)
+      .clickable(onClickLabel = "make-new-friends") { handleClick() }
+      .padding(horizontal = 12.dp)
+  ) {
+    // Avatar
+    Image(
+      painter = painterResource(R.drawable.cherry_blossom_background_200x150),
+      contentDescription = "cherry blossoms",
+      contentScale = ContentScale.FillBounds,
+      modifier = Modifier
+        .size(50.dp)
+        // Orange circle
+        .clip(RoundedCornerShape(8.dp))
+    )
+
+    Text(
+      text = "Make new friends!",
+      style = MaterialTheme.typography.h6,
+      modifier = Modifier.padding(start = 12.dp)
+    )
+  }
+}
+
+@Preview(widthDp = 360)
+@Composable
+fun MakeNewFriendsCardPreview() {
+  ShibachatAndroidTheme {
+    Surface {
+      MakeNewFriendsCard {}
+    }
+  }
+}
---
+++ app/src/main/java/one/beefsupreme/shibachatandroid/ui/friendsscreen/ReceivedFReqCard.kt
@@ -0,0 +1,98 @@
+package one.beefsupreme.shibachatandroid.ui.friendsscreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import one.beefsupreme.shibachatandroid.MeQuery
+import one.beefsupreme.shibachatandroid.R
+import one.beefsupreme.shibachatandroid.ui.theme.ShibachatAndroidTheme
+
+@Composable
+fun ReceivedFReqCard(user: MeQuery.ReceivedFRequest) {
+  Row(
+    horizontalArrangement = Arrangement.SpaceBetween,
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = Modifier.fillMaxWidth()
+      .height(65.dp)
+      .padding(horizontal = 12.dp)
+  ) {
+    // Avatar
+    Image(
+      painter = painterResource(R.drawable.cherry_blossom_background_200x150),
+      contentDescription = "cherry blossoms",
+      contentScale = ContentScale.FillBounds,
+      modifier = Modifier
+        .size(50.dp)
+        // Orange circle
+        .clip(RoundedCornerShape(8.dp))
+    )
+
+    Text(
+      text = "${user.nickname} wants to be your friend. Do you accept?",
+      style = MaterialTheme.typography.caption,
+      modifier = Modifier.width(140.dp)
+    )
+
+    Button(
+      onClick = {},
+      shape = CircleShape,
+    ) {
+      Text(
+        text = "Yes!",
+        modifier = Modifier.width(40.dp),
+        textAlign = TextAlign.Center
+      )
+    }
+
+    TextButton(
+      onClick = {},
+      shape = MaterialTheme.shapes.small,
+      colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.onSurface)
+    ) {
+      Text(
+        text = "DO NOT WANT!",
+        style = MaterialTheme.typography.caption,
+        modifier = Modifier.width(50.dp),
+        textAlign = TextAlign.Center
+      )
+    }
+  }
+}
+
+@Preview(widthDp = 360)
+@Composable
+fun ReceivedFReqCardPreview() {
+  ShibachatAndroidTheme {
+    Surface {
+      ReceivedFReqCard(
+        MeQuery.ReceivedFRequest(
+          id = 3,
+          nickname = "Dwayne Johnson",
+          __typename = "User"
+        )
+      )
+    }
+  }
+}
