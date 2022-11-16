b3655f3fe0c35edc6bc7191e56f46e5b0f17e390
onions41 <kblee89@outlook.com>
2022-11-16 15:55:24 -0800

    Created FRequestsScreen

--- app/src/main/java/one/beefsupreme/shibachatandroid/ui/friendsscreen/FRequestsScreen.kt
+++ app/src/main/java/one/beefsupreme/shibachatandroid/ui/friendsscreen/FRequestsScreen.kt
@@ -1,33 +1,28 @@
 package one.beefsupreme.shibachatandroid.ui.friendsscreen
 
-import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
-import one.beefsupreme.shibachatandroid.repo.MeQueryState
 import one.beefsupreme.shibachatandroid.ui.FriendsNavGraph
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import one.beefsupreme.shibachatandroid.ui.destinations.FRequestsScreenDestination
 
-private const val TAG = "**FriendsScreen**"
-
-@FriendsNavGraph(start = true)
+@FriendsNavGraph
 @Destination
 @Composable
-fun FriendsScreen(
-  vm: FriendsViewModel = hiltViewModel(),
-  navigator: DestinationsNavigator
+fun FRequestsScreen(
+  vm: FriendsViewModel = hiltViewModel()
 ) {
   val meState = vm.meFetch.state
-  Log.v(TAG, meState.toString())
+  val state by vm.state.collectAsState()
 
   Surface(
     modifier = Modifier.fillMaxSize()
@@ -35,17 +30,17 @@ fun FriendsScreen(
     LazyColumn(
       modifier = Modifier.padding(vertical = 4.dp)
     ) {
-      item(key = "make-new-friends-card") {
-        MakeNewFriendsCard { navigator.navigate(FRequestsScreenDestination) }
-      }
 
-      if (meState is MeQueryState.Success) {
-        items(items = meState.me.receivedFRequests) { user ->
-          ReceivedFReqCard(user)
+      if (state is FReqUiState.Success) {
+        items(
+          items = (state as FReqUiState.Success).allUsers,
+          key = { user -> user.id }
+        ) { user ->
+          if (true) { UserCard(user) }
         }
       } else {
         item(key = "loading-indicator") {
-          Text("Me is still loading or it failed")
+          Text("AllUsers query is either still loading or it failed")
         }
       }
     }
