54dc3db5aca72880b6a3537747ed5f22cb8b9d0e
onions41 <kblee89@outlook.com>
2022-11-22 16:28:45 -0800

    SendFRequests of FRequestsViewModel updates the MeQuery and AllUsers cache

--- app/src/main/java/one/beefsupreme/shibachatandroid/ui/friendsscreen/FRequestsViewModel.kt
+++ app/src/main/java/one/beefsupreme/shibachatandroid/ui/friendsscreen/FRequestsViewModel.kt
@@ -8,19 +8,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.cache.normalized.FetchPolicy
+import com.apollographql.apollo3.cache.normalized.apolloStore
 import com.apollographql.apollo3.cache.normalized.fetchPolicy
+import com.apollographql.apollo3.cache.normalized.watch
 import com.apollographql.apollo3.exception.ApolloException
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import one.beefsupreme.shibachatandroid.AllUsersQuery
 import one.beefsupreme.shibachatandroid.AppDispatchers
+import one.beefsupreme.shibachatandroid.MeQuery
 import one.beefsupreme.shibachatandroid.SendFRequestMutation
+import one.beefsupreme.shibachatandroid.repo.LoginState
 import one.beefsupreme.shibachatandroid.repo.Me
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 private const val TAG = "**FRequestsViewModel**"
@@ -47,6 +51,7 @@ sealed class SendFRequestResult {
 class FRequestsViewModel @Inject constructor(
   private val apolloClient: ApolloClient,
   private val appDispatchers: AppDispatchers,
+  val loginState: LoginState,
   val me: Me
 ): ViewModel() {
   // Has to be MutableStateFlow because its value is changed in the init block
@@ -73,26 +78,29 @@ class FRequestsViewModel @Inject constructor(
       // in order for ApolloClient to throw both network errors and errors thrown in
       // the backend resolvers. Otherwise, ApolloClient will return an errors object that
       // contains the resolver errors separately. I don't want them separately at the moment.
-      _allUsersResult.value = try {
-        val data = apolloClient
-          .query(AllUsersQuery())
-          .fetchPolicy(FetchPolicy.NetworkOnly)
-          .execute()
-          .dataAssertNoErrors // Because I want apolloClient to throw all errors
-
-        // Fetch was successful.
-        // Login mutation always returns an access token in this case.
-        AllUsersResult.Success(data)
-      } catch (error: ApolloException) {
-        AllUsersResult.Failed(error)
-      }
+      apolloClient
+        .query(AllUsersQuery())
+        .fetchPolicy(FetchPolicy.NetworkOnly)
+        .watch()
+        .map {
+          val data = it.data
+          if (data?.users == null) {
+            AllUsersResult.Failed(ApolloException("what happened?"))
+          } else {
+            AllUsersResult.Success(data)
+          }
+        }
+        .collect {
+          ensureActive()
+          _allUsersResult.value = it
+        }
     }
 
     Log.v(TAG, "AllUsersQuery just ran")
   }
 
   private fun sendFRequest(friendId: Int) {
-    // Needs to use mutex to protect against multiple button clicks after protecting it UI wise first
+    // TODO: Needs to use mutex to protect against multiple button clicks after protecting it UI wise first
     viewModelScope.launch(appDispatchers.io) {
       sendFRequestResult = SendFRequestResult.Loading
 
@@ -101,15 +109,76 @@ class FRequestsViewModel @Inject constructor(
           .mutation(SendFRequestMutation(friendId))
           .execute()
           .dataAssertNoErrors
+        // Mutation was successful
+
+        /** Modifying MeQuery cache (SentFRequest) */
+        val ( // Grabs all properties of data.sendFRequest
+          __typename,
+          requesterId,
+          requesteeId,
+          sentFRequestFragment
+        ) = data.sendFRequest // is SendFRequestMutation.SendFRequest
+
+        // Uses above properties to construct a new MeQuery.SentFRequest
+        // This works because MeQuery.SentFRequest has the same shape
+        // as SendFRequestMutation.SendFRequest
+        val newSentFRequest = MeQuery.SentFRequest(
+          __typename,
+          requesterId,
+          requesteeId,
+          sentFRequestFragment
+        )
+
+        // Reads MeQuery data out of the cache.
+        var meData = apolloClient.apolloStore.readOperation(MeQuery())
+        // Turns it into a mutable list
+        val sentFRequests = meData.user.sentFRequests.toMutableList()
+        // Appends newSentFRequest the mutable list
+        sentFRequests.add(newSentFRequest)
+        // Alters meData by replacing the sentFRequests list with the new list
+        meData = meData.copy(
+          user = meData.user.copy(
+            sentFRequests = sentFRequests.toList()
+          )
+        )
+
+        // Write the altered MeQuery data that includes the just sent FRequest
+        // into the cache
+        apolloClient.apolloStore.writeOperation(
+          operation = MeQuery(),
+          operationData = meData
+        )
+
+        /**
+         * Modifying AllUsersQuery cache by changing one of the user's
+         * receivedFReqFromMe property to true
+         */
+        val allUsersData = apolloClient.apolloStore.readOperation(AllUsersQuery())
+
+        // Replaces the user element to whom the friend request was sent with the
+        // copy of that element with the property receivedFReqFromMe changed to true
+        val users = allUsersData.users.toMutableList()
+        val i = users.indexOfFirst { it.id == friendId }
+        if (i >= 0 ) { // Above returns -1 if not found
+          val friend = users.removeAt(i)
+          users.add(i, friend.copy(receivedFReqFromMe = true))
+        }
+
+        apolloClient.apolloStore.writeOperation(
+          operation = AllUsersQuery(),
+          operationData = allUsersData.copy(users = users.toList())
+        )
 
         SendFRequestResult.Success(data)
       } catch (error: ApolloException) {
+        Log.e(TAG, error.message.toString())
         SendFRequestResult.Failed(error)
       }
 
-      // SendFRequestResult needs to be flipped back to Ready before the buttons will work again.
-      // delays for 3/4 of a second, then flips it back to Ready. This delay is really needed.
-      delay(TimeUnit.MILLISECONDS.toMillis(750))
+      // For slowing down UI to take a closer look at the loading UI
+//      delay(TimeUnit.MILLISECONDS.toMillis(750))
+
+      // Flips the result object back to Ready
       sendFRequestResult = SendFRequestResult.Ready
     }
   }
